6114f0c88d60018c1c247e0bc5bc601af6164e09
Nicoliiii <119983473+Nicoliiii@users.noreply.github.com>
2023-12-10 20:13:07 +0000

    fix:config02

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,40 @@
+// page.tsx
+import { useClient } from 'next/data-client';
+import Cadastro from './cadastro';
 
 export default function Home() {
+  const { data: livros } = useClient().useState([]); // Use useClient here
+
+  const [mostrarCadastro, setMostrarCadastro] = livros.useState(false);
+
+  const handleSalvarLivro = (livro) => {
+    livros.setState([...livros(), livro]);
+    setMostrarCadastro(false);
+  };
+
+  const handleCancelarCadastro = () => {
+    setMostrarCadastro(false);
+  };
+
   return (
-    <h1>teste</h1>
-     )
+    <div>
+      <h1>Lista de Livros</h1>
+      <ul>
+        {livros().map((livro, index) => (
+          <li key={index}>
+            {livro.nome} - {livro.numPaginas} páginas
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={() => setMostrarCadastro(true)}>Cadastro</button>
+
+      {mostrarCadastro && (
+        <Cadastro
+          onSalvar={handleSalvarLivro}
+          onCancel={handleCancelarCadastro}
+        />
+      )}
+    </div>
+  );
 }
---
+++ components/cadastro.tsx
@@ -0,0 +1,43 @@
+// Cadastro.tsx
+import { useState } from 'react';
+
+export default function Cadastro({
+  onSalvar,
+  onCancel,
+}: {
+  onSalvar: (livro: { nome: string; numPaginas: number }) => void;
+  onCancel: () => void;
+}) {
+  const [nome, setNome] = useState('');
+  const [numPaginas, setNumPaginas] = useState('');
+
+  const handleSalvar = () => {
+    onSalvar({ nome, numPaginas: parseInt(numPaginas, 10) });
+    setNome('');
+    setNumPaginas('');
+  };
+
+  return (
+    <div>
+      <h2>Cadastro</h2>
+      <label htmlFor="nomeLivro">Nome do Livro:</label>
+      <input
+        type="text"
+        id="nomeLivro"
+        value={nome}
+        onChange={(e) => setNome(e.target.value)}
+      />
+
+      <label htmlFor="numPaginas">Número de Páginas:</label>
+      <input
+        type="number"
+        id="numPaginas"
+        value={numPaginas}
+        onChange={(e) => setNumPaginas(e.target.value)}
+      />
+
+      <button onClick={handleSalvar}>Salvar</button>
+      <button onClick={onCancel}>Cancelar</button>
+    </div>
+  );
+}
